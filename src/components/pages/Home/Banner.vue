49055c525b7b3d6718732c289ecacf0dc1f80d2a
Z-LianYan <1371935318@qq.com>
2018-08-08 04:00:19 +0800

    修改了banner组件

--- src/components/pages/Home/Banner.vue
+++ src/components/pages/Home/Banner.vue
@@ -33,6 +33,8 @@ export default {
             this.banners = res.data.data.billboards
             Vue.nextTick(() => {
                 new Swiper('.app-banner', {
+                	autoplay:true,
+                	loop:true,
                     pagination: {
                         el: '.swiper-pagination'
                     },
